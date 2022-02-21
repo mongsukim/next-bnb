ba0b2a9118d39eea51ecbf159a5296da03241e11
DESKTOP-O5M0547\kimhyesu <developermong2@gmail.com>
2022-02-21 17:09:03 +0900

    스타일 컴포넌트, svg 컴포넌트 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Styled from "styled-components";
+
+const Container = styled.div`
+    font-size: 21px;
+    color:gray;
+`;
 
 const index : React.FC = () => {
-    return <div>hello</div>;
+    return <Container>hello</Container>;
 };
 
 export default index;
